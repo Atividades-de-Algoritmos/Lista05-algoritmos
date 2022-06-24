d68d2850f3e64ac7c153cc50ebe86ea0007ee481
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 21:22:11 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,16 @@
+#
+# 1.	Desenvolva um programa em Python que recebe do usuário um número real e exibe em tela se este número é maior,
+# menor ou igual que zero.
+#
+# entrada de dados
+numero = float(input("Digite um número real: ")) # Recebe o número
+
+# processamento
+if numero > 0:
+  print(f"{numero} é maior que zero") # Imprime "Maior que zero"
+elif numero < 0:
+  print(f"{numero} é menor que zero") # Imprime "Menor que zero"
+else:
+  print(f"{numero} é igual a zero") # Imprime "Igual a zero"
+
+print("Fim do programa")
