d958b0d1cd1a23f3a46bd288541a80f2a403da3a
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-27 23:38:58 -0300

    Conversor de temperaturas 🌡

--- ex04.py
+++ ex04.py
@@ -4,7 +4,7 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 4. Crie um programa em linguagem Python que recebe do      
 # usuário um valor de temperatura em graus Celsius e o       
@@ -28,15 +28,40 @@
 # K = C + 273.15 # Equação para converter Kelvin para Celsius
 
 # Entrada de dados
+
 opcao = input("Digite F para converter de Celsius para Fahrenheit ou K para converter de Celsius para Kelvin: ") # Recebe a opção de conversão
 valor = float(input("Digite o valor da temperatura: ")) # Recebe o valor da temperatura
 
-# Processamento
+# Processamento e saída de dados
+
 if opcao == "F" or opcao == "f": # Se a opção for F ou f
   print(f"\n{valor} Celsius é igual a {(valor - 32) * 5/9} Fahrenheit") # Imprime o valor convertido para Fahrenheit
+
 elif opcao == "K" or opcao == "k": # Caso contrário, se a opção for K ou k
   print(f"\n{valor} Celsius é igual a {valor + 273.15} Kelvin") # Imprime o valor convertido para Kelvin
+
 else: # Caso contrário, se a opção for qualquer outra letra
   print("Opção inválida de conversão!") # Imprime a mensagem de opção inválida
 
 print("fim do programa") # Informa ao usuário que o programa terminou
+
+# Versão 2.0 do código
+
+# ---------------------------------------------------------------------------------- #
+
+# Entrada de dados (Mesma da primeira versão)
+
+# Processamento e saída de dados
+
+# if opcao.lower() == 'f': # .lower() coloca toda a string em mínusculas, ex: 'CARLOS' depois do .lower() vira 'carlos'
+  # print(f"\n{valor} Celsius é igual a {(valor - 32) * 5/9} Fahrenheit")
+
+# elif opcao.lower() == 'k':  # .lower() coloca toda a string em mínusculas, ex: 'CARLOS' depois do .lower() vira 'carlos'
+  # print(f"\n{valor} Celsius é igual a {valor + 273.15} Kelvin")
+
+# else: # Caso contrário, se a opção for qualquer outra letra
+  # print("Opção inválida de conversão!") # Imprime a mensagem de opção inválida
+
+# print("fim do programa")
+
+# ---------------------------------------------------------------------------------- #
